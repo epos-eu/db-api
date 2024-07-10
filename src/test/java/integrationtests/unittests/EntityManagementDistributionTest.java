d4efcec2e0dd4abcef2315b78427cc04d2811d12
Valerio Vinciarelli <valerio.vinciarelli@gmail.com>
2024-07-10 19:06:29 +0200

    added tests and fixed issues

---
+++ src/test/java/integrationtests/unittests/EntityManagementDistributionTest.java
@@ -0,0 +1,45 @@
+package integrationtests.unittests;
+
+import abstractapis.AbstractAPI;
+import integrationtests.TestcontainersLifecycle;
+import metadataapis.EntityNames;
+import org.eclipse.persistence.internal.jpa.rs.metadata.model.Link;
+import org.epos.eposdatamodel.Address;
+import org.epos.eposdatamodel.Distribution;
+import org.epos.eposdatamodel.LinkedEntity;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class EntityManagementDistributionTest extends TestcontainersLifecycle {
+
+    @Test
+    @Order(1)
+    public void testCreateAndGetAddress() {
+
+        AbstractAPI api = AbstractAPI.retrieveAPI(EntityNames.DISTRIBUTION.name());
+
+        LinkedEntity le = new LinkedEntity();
+        le.setUid("test");
+        le.setEntityType(EntityNames.WEBSERVICE.name());
+
+        Distribution distribution = new Distribution();
+        distribution.setInstanceId(UUID.randomUUID().toString());
+        distribution.setMetaId(UUID.randomUUID().toString());
+        distribution.setUid(UUID.randomUUID().toString());
+        distribution.setAccessService(le);
+
+        api.create(distribution, null);
+
+        Distribution retrievedDistribution = (Distribution) api.retrieve(distribution.getInstanceId());
+
+        assertNotNull(retrievedDistribution);
+    }
+
+
+}
